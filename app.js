261d568e08d54007688e691f13645ba4e8f081bd
khushal <khushalsharma40312@gmail.com>
2025-02-28 12:03:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ async function init() {
 
   // Create Apollo Server instance with schema
   const { gqlServer, schema } = await CreateApollo();
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing",
+});
 
 
   app.use(
